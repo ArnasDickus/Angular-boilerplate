21e528f7802f9f604d365ee3f46e5b30d6bc2ca2
Arnas <1arnasdickus1@gmail.com>
2020-08-27 14:36:39 +0300

    fix: boilerplate created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    loadChildren: () => import('./features/not-found/not-found.module').then(m => m.NotFoundModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
